602ac69984f8c81d0225676beb0bf759b3c29f67
bastardijke <ruthlessbastard@gmail.com>
2018-01-31 17:26:59 +0500

    Initial

---
+++ SHARP/P1X/AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/P1X.mk \
+	$(LOCAL_DIR)/omni_P1X.mk
---
+++ SHARP/P1X/omni_P1X.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/SHARP/P1X/P1X.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := P1X
+PRODUCT_NAME := omni_P1X
+PRODUCT_BRAND := SHARP
+PRODUCT_MODEL := AQUOS P1
+PRODUCT_MANUFACTURER := SHARP
